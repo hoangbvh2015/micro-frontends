c865556117ca614ea364edbb6b5bf0c746b11788
Hoang <hoangbvh2015@gmail.com>
2021-09-13 08:43:18 +0700

    delete calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,6 @@ const routes: Routes = [
   {
     path: 'mailbox',
     loadChildren: () => import('mailbox/MailboxModule').then(m => m.MailboxModule)
-  },
-  {
-    path: 'calendar',
-    loadChildren: () => import('calendar/CalendarModule').then(m => m.CalendarModule)
   }
 ];
 @NgModule({
--- src/app/types.d.ts
+++ src/app/types.d.ts
@@ -1,2 +1,1 @@
 declare module 'mailbox/MailboxModule';
-declare module 'calendar/CalendarModule';
